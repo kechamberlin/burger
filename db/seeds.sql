4f25b9de98bddddd1426e779a6fa6019aa2b13db
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2019-10-28 22:32:59 -0700

    added false value to column names

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,4 +1,4 @@
-INSERT INTO burgers (burger_name, devoured) VALUES ("Double-Double Cheeseburger"),
+INSERT INTO burgers (burger_name, devoured) VALUES ("Double-Double Cheeseburger", flase),
 INSERT INTO burgers (burger_name, devoured) VALUES ("Cowboy Burger", false),
 INSERT INTO burgers (burger_name, devoured) VALUES ("Impossible Burger", false),
 
