22d70259e8af6ca6e9de1eff416d94597680783f
Kris Chamberlin <kris.elliott.chamberlin@gmail.com>
2019-11-15 14:49:14 -0800

    commit

--- config/promisify-mysql.js
+++ config/promisify-mysql.js
@@ -3,19 +3,19 @@ const { getConnection } = require("./connection");
 async function query(...inputs) {
     // ensure every time we query we have a connection
     const connection = await getConnection();
-    return new Promise((resolve, reject) => {
+    return (new Promise((resolve, reject) => {
       console.log(inputs)
-    const yabba = connection.query(...inputs, 
+      connection.query(...inputs, 
         function(err, results) {
-          console.log(yabba.sql);
+          console.log(err);
           if (err) { 
             console.error(err);
             return reject(err);
           }
           resolve(results);
         }
-      ).catch(error => console.err(error));
-    })
+      )
+    })).catch(err => console.error(err));
 }
 
 async function end() {
